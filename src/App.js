4432d5e787e70f5941b322caccb2c83b9f1fb324
allenyonemoto <allenyonemoto@icloud.com>
2024-07-05 00:34:47 -0400

    Changes done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import LandingPage from './Components/Landing_Page/Landing_Page';
 import SignUp from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearch from './Components/BookingConsultation/FindDoctorSearch/FindDoctorSearch';
+import BookingConsultation from './Components/BookingConsultation/BookingConsultation';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
             <Route path="/login" element={<Login />} /> 
             <Route path="/signup" element={<SignUp />} /> 
             <Route path="/instant-consultation" element={<InstantConsultation />} />
+            <Route path="/booking-consultation" element={<BookingConsultation />} /> {/* Route for searching doctors */}
+            <Route path='/finddoctor' element={<FindDoctorSearch />} />
         </Routes>
         </BrowserRouter>
     </div>
