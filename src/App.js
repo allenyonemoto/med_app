974ae380c52ad948f4987675954addd3bf6ecbdc
allenyonemoto <allenyonemoto@icloud.com>
2024-07-05 08:13:08 -0400

    Changes done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-// import React, { useEffect } from 'react';
+//import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/Landing_Page/Landing_Page';
